1e459744e61d27f159b5460397c9c53f905e0899
Israel Castro <israel.castro@gmail.com>
2021-01-11 14:26:55 -0600

    [Add] adsense

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,6 +56,12 @@ module.exports = {
          *  Content Plugins
          */
         {
+            resolve: `gatsby-plugin-google-adsense`,
+            options: {
+                publisherId: `ca-pub-7144863238088863`
+            },
+        },
+        {
             resolve: `gatsby-source-filesystem`,
             options: {
                 path: path.join(__dirname, `src`, `pages`),
@@ -200,43 +206,43 @@ module.exports = {
         `gatsby-plugin-catch-links`,
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-force-trailing-slashes`,
-				`gatsby-plugin-offline`,
-				{
-					resolve: `gatsby-transformer-remark`,
-					options: {
-						plugins: [
-							{
-								resolve: `gatsby-remark-prismjs`,
-								options: {
-									classPrefix: "language-",
-									inlineCodeMarker: null,
-									aliases: {},
-									showLineNumbers: false,
-									noInlineHighlight: false,
-									languageExtensions: [
-										{
-											language: "superscript",
-											extend: "javascript",
-											definition: {
-												superscript_types: /(SuperType)/,
-											},
-											insertBefore: {
-												function: {
-													superscript_keywords: /(superif|superelse)/,
-												},
-											},
-										},
-									],
-									prompt: {
-										user: "root",
-										host: "localhost",
-										global: false,
-									},
-									escapeEntities: {},
-								},
-							},
-						],
-					},
-				},
+        `gatsby-plugin-offline`,
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-prismjs`,
+                        options: {
+                            classPrefix: "language-",
+                            inlineCodeMarker: null,
+                            aliases: {},
+                            showLineNumbers: false,
+                            noInlineHighlight: false,
+                            languageExtensions: [
+                                {
+                                    language: "superscript",
+                                    extend: "javascript",
+                                    definition: {
+                                        superscript_types: /(SuperType)/,
+                                    },
+                                    insertBefore: {
+                                        function: {
+                                            superscript_keywords: /(superif|superelse)/,
+                                        },
+                                    },
+                                },
+                            ],
+                            prompt: {
+                                user: "root",
+                                host: "localhost",
+                                global: false,
+                            },
+                            escapeEntities: {},
+                        },
+                    },
+                ],
+            },
+        },
     ],
 }
