3ba4693ea713214ee1f285079e82c2edd4dadadd
Israel Castro <israel.castro@gmail.com>
2021-07-13 12:55:26 -0500

    style(referral): add referral link
    This change includes referral widget in each post

---
+++ src/components/common/post/Referral.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Referral() {
+    return (
+        <>
+            <section className="referral">
+                <p>
+                    Entré a <a href="https://isca.dev/platzi">Platzi</a> en 2019
+                    y en esta pandamia logré 50+ certificaciones (en desarrollo
+                    web, desarollo personal e inglés). Si te animas crecer
+                    profesionalmente o cambiar de carrera como yo, te dejo un{" "}
+                    <strong>código exclusivo con un 30% de descuento</strong>{" "}
+                    para 2 personas (válido hasta el 31/07/2021).
+                </p>
+                <p>
+                    <a className="btn" href="https://isca.dev/platzi">
+                        Obtener descuento
+                    </a>
+                </p>
+            </section>
+        </>
+    );
+}
+
+export default Referral;
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,77 +1,119 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
-import { Helmet } from 'react-helmet'
-import { Tags } from '@tryghost/helpers-gatsby'
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import { Helmet } from "react-helmet";
+import { Tags } from "@tryghost/helpers-gatsby";
 
-import { Layout } from '../components/common'
-import { MetaData } from '../components/common/meta'
+import { Layout } from "../components/common";
+import { MetaData } from "../components/common/meta";
+
+import Referral from "../components/common/post/Referral";
 
 /**
-* Single post view (/:slug)
-*
-* This file renders a single post and loads all the content.
-*
-*/
+ * Single post view (/:slug)
+ *
+ * This file renders a single post and loads all the content.
+ *
+ */
 const Post = ({ data, location }) => {
-    const post = data.ghostPost
-    post.childHtmlRehype.html = post.childHtmlRehype.html.replace(/<a/g, `<a target="_blank" rel="noopener noreferrer"`)
+    const post = data.ghostPost;
+    post.childHtmlRehype.html = post.childHtmlRehype.html.replace(
+        /<a/g,
+        `<a target="_blank" rel="noopener noreferrer"`
+    );
 
     return (
         <>
-            <MetaData
-                data={data}
-                location={location}
-                type="article"
-            />
+            <MetaData data={data} location={location} type="article" />
             <Helmet>
                 <style type="text/css">{`${post.codeinjection_styles}`}</style>
             </Helmet>
             <Layout>
                 <div className="container">
                     <article className="content">
-                    <header class="post-full-header">
-                        {post.tags && <div className="post-card-tags"> <Tags post={post} visibility="public" autolink={false} /></div>}
-                        <h1 className="content-title">{post.title}</h1>
-                        <p class="post-full-custom-excerpt">{post.excerpt}</p>
-                        <div className="post-full-byline-meta">
-                            <div>
-                                <ul class="author-list">
-                                    <li class="author-list-item">
-                                        <div class="author-card">
-                                            {post.primary_author.profile_image ?
-                                                <img className="author-avatar" src={post.primary_author.profile_image} alt={post.primary_author.name}/> :
-                                                <img className="default-avatar" src="/images/icons/avatar.svg" alt={post.primary_author.name}/>
-                                            }
-                                        </div>
-                                    </li>
-                                </ul>
+                        <header class="post-full-header">
+                            {post.tags && (
+                                <div className="post-card-tags">
+                                    {" "}
+                                    <Tags
+                                        post={post}
+                                        visibility="public"
+                                        autolink={false}
+                                    />
+                                </div>
+                            )}
+                            <h1 className="content-title">{post.title}</h1>
+                            <p class="post-full-custom-excerpt">
+                                {post.excerpt}
+                            </p>
+                            <div className="post-full-byline-meta">
+                                <div>
+                                    <ul class="author-list">
+                                        <li class="author-list-item">
+                                            <div class="author-card">
+                                                {post.primary_author
+                                                    .profile_image ? (
+                                                    <img
+                                                        className="author-avatar"
+                                                        src={
+                                                            post.primary_author
+                                                                .profile_image
+                                                        }
+                                                        alt={
+                                                            post.primary_author
+                                                                .name
+                                                        }
+                                                    />
+                                                ) : (
+                                                    <img
+                                                        className="default-avatar"
+                                                        src="/images/icons/avatar.svg"
+                                                        alt={
+                                                            post.primary_author
+                                                                .name
+                                                        }
+                                                    />
+                                                )}
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div>
+                                    <h4 className="author-name">
+                                        {post.primary_author.name}
+                                    </h4>
+                                    <span className="byline-meta-date">
+                                        {post.created_at_pretty}
+                                    </span>
+                                </div>
                             </div>
-                            <div>
-                                <h4 className="author-name">{ post.primary_author.name }</h4>
-                                <span className="byline-meta-date">{ post.created_at_pretty }</span>
-                            </div>
-                        </div>
                         </header>
 
-                        { post.feature_image ?
-                        <figure className="post-feature-image">
-                            <img src={ post.feature_image } alt={ post.title } />
-                        </figure> : null }
+                        {post.feature_image ? (
+                            <figure className="post-feature-image">
+                                <img
+                                    src={post.feature_image}
+                                    alt={post.title}
+                                />
+                            </figure>
+                        ) : null}
 
                         <section className="post-full-content">
-                            {/* The main post content */ }
+                            {/* The main post content */}
                             <section
                                 className="content-body load-external-scripts"
-                                dangerouslySetInnerHTML={{ __html: post.childHtmlRehype.html }}
+                                dangerouslySetInnerHTML={{
+                                    __html: post.childHtmlRehype.html,
+                                }}
                             />
+                            <Referral />
                         </section>
                     </article>
                 </div>
             </Layout>
         </>
-    )
-}
+    );
+};
 
 Post.propTypes = {
     data: PropTypes.shape({
@@ -87,9 +129,9 @@ Post.propTypes = {
         }).isRequired,
     }).isRequired,
     location: PropTypes.object.isRequired,
-}
+};
 
-export default Post
+export default Post;
 
 export const postQuery = graphql`
     query($slug: String!) {
@@ -97,4 +139,4 @@ export const postQuery = graphql`
             ...GhostPostFields
         }
     }
-`
+`;
